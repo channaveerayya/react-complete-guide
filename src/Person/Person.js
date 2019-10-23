a314764b4a3a05e27759882331565bdcce4ee40e
channaveerayya <channucs0009@gmail.com>
2019-10-23 15:09:04 +0530

    Adding inline Style and Stylesheet

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import './Person.css'
 const Person = (props) => {
+    const inlineStyle = {
+      backgroundColor: "red",
+      border: "1px solid blue",
+      padding: "8px",
+      color: "white",
+      cursor: " pointer"
+    };
     return (
-        <div>
+        <div className="Person">
             <p>Person functional component</p>
-            <button onClick={props.clickRef}> click </button>
+            <button onClick={props.clickRef} style={inlineStyle}> click </button>
             <h1>name={props.name} age={props.age}</h1>
             <input type="text" onChange={props.change} defaultValue={props.name}/>
         </div>
