5b3ce089810506f9ad3fc3dd74f2c7dce0a403d4
channaveerayya <channucs0009@gmail.com>
2019-10-24 17:16:11 +0530

    setting setState correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
       { id: 'asdf11', name: 'Dinu', age: 26 }
     ],
     otherState: 'some other value',
-    showPersons: false
+    showPersons: false,
+    changeCounter: 0
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -64,7 +65,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState,props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
   };
 
   deletePersonHandler = personIndex => {
