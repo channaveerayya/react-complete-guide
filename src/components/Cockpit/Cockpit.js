fbf97a8bf99f35f7d40b08d8ff2de5b905a8b19e
channaveerayya <channucs0009@gmail.com>
2019-10-25 10:50:25 +0530

    ref used in functional component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import "./Cockpit.css"
 
+const Cockpit = props => {
 
-const cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
 
+  useEffect(() => {
+    toggleBtnRef.current.click();
+    return () => {
+
+    }
+  }, []);
 
   return (
     <div className="Cockpit">
       <h1>{props.title}</h1>
       <p>This is really working!</p>
       <button
+        ref={toggleBtnRef}
         onClick={props.clicked}>Toggle Persons</button>
     </div>
   );
 };
 
-export default cockpit;
+export default Cockpit;
